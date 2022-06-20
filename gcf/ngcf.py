d3c5c8735ee1ee11847d21f19134bbec5b863428
Stalin Sabu Thomas <44336699+legendof-selda@users.noreply.github.com>
2022-06-20 15:34:14 +0530

    fix: named weights dict

--- gcf/ngcf.py
+++ gcf/ngcf.py
@@ -2,7 +2,16 @@ from .base import BaseCF
 
 
 class NGCF(BaseCF):
-    def __init__(self, n_users, n_items, n_cat, n_fold=100, emb_propogation_layers=[64], node_dropout=False, **kwargs):
+    def __init__(
+        self,
+        n_users,
+        n_items,
+        n_cat,
+        n_fold=100,
+        emb_propogation_layers=[64],
+        node_dropout=False,
+        **kwargs,
+    ):
         self.emb_propogation_layers = emb_propogation_layers
         self.n_propogating_layers = len(self.emb_propogation_layers)
         self.node_dropout = node_dropout
@@ -19,14 +28,24 @@ class NGCF(BaseCF):
         item_emb_prop = [self.item_emb_size] + self.emb_propogation_layers
 
         for layer in range(self.n_propogating_layers):
-            var = tf.Variable(self.initializer(user_emb_prop[layer], user_emb_prop[layer + 1]), name=f"weight_user_propogated_L{layer}")
-            self.propogation_layers_weights[var.name] = var
+            name = f"weight_user_propogated_L{layer}"
+            self.propogation_layers_weights[name] = tf.Variable(
+                self.initializer(user_emb_prop[layer], user_emb_prop[layer + 1]),
+                name=name,
+            )
 
-            var = tf.Variable(self.initializer(1, user_emb_prop[layer + 1]), name=f"bias_user_propogated_L{layer}")
-            self.propogation_layers_weights[var.name] = var
+            name = f"bias_user_propogated_L{layer}"
+            self.propogation_layers_weights[name] = tf.Variable(
+                self.initializer(1, user_emb_prop[layer + 1]), name=name
+            )
 
-            var = tf.Variable(self.initializer(item_emb_prop[layer], item_emb_prop[layer + 1]), name=f"weight_item_propogated_L{layer}")
-            self.propogation_layers_weights[var.name] = var
+            name = f"weight_item_propogated_L{layer}"
+            self.propogation_layers_weights[name] = tf.Variable(
+                self.initializer(item_emb_prop[layer], item_emb_prop[layer + 1]),
+                name=name,
+            )
 
-            var = tf.Variable(self.initializer(1, item_emb_prop[layer + 1]), name=f"bias_item_propogated_L{layer}")
-            self.propogation_layers_weights[var.name] = var
+            name = f"bias_item_propogated_L{layer}"
+            self.propogation_layers_weights[name] = tf.Variable(
+                self.initializer(1, item_emb_prop[layer + 1]), name=name
+            )
